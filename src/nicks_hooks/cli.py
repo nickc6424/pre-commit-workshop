b009657969f6ecb3a06108808d8ad2dd7c8699a4
Nick Cole <Nick.Cole@nanoporetech.com>
2024-07-08 16:25:42 +0200

    Created my own hook.

---
+++ src/nicks_hooks/cli.py
@@ -0,0 +1,48 @@
+import argparse
+from typing import Sequence
+
+from src.nicks_hooks import filename_length_hook
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    """
+    CLI entrypoint.
+
+    Parameters
+    ----------
+    argv : Sequence[str] | None
+           Command line arguments.
+
+    Returns
+    ----------
+    int
+    1 if the check finds a problem, otherwise 0
+    """
+    parser = argparse.ArgumentParser(prog='validate-filename-length')
+    parser.add_argument(
+        name='filenames',
+        nargs='*',
+        help='Filenames to process.',
+    )
+    parser.add_argument(
+        name='--min-len',
+        required=False,
+        default=3,
+        help='The minimum allowed length for filenames.',
+    )
+    parser.add_argument(
+        name='--max-len',
+        required=False,
+        default=40,
+        help='The maximum allowed length for filenames.',
+    )
+
+    args = parser.parse_args(argv)
+
+    results = [
+        filename_length_hook.is_filename_length_valid(
+            filename, args.min_len, args.max_len
+        )
+        for filename in args.filenames
+    ]
+    return int(all(results))
--- src/nicks_hooks/filename_length_hook.py
+++ src/nicks_hooks/filename_length_hook.py
@@ -1,14 +1,36 @@
-LOWER_LIMIT = 3
-UPPER_LIMIT = 15
+"""Functions to support the filename length hook."""
+
+from pathlib import Path
 
 
-def perform_check(filename: str) -> int:
-    filename_length = len(filename)
-    too_long = filename_length > UPPER_LIMIT
-    too_short = filename_length < LOWER_LIMIT
+def is_filename_length_valid(filename: str, min_length: int, max_length: int) -> int:
+    """
+    Check if a filename length is valid.
+
+    Parameters
+    ----------
+    filename : str
+               The name of the file to be checked.
+    min_length : int
+                 The minimum permitted length.
+    max_length : int
+                 The maximum permitted length.
+
+    Returns
+    ----------
+    is_valid: bool
+              True if valid, False if invalid.
+    """
+    filename_length = len(Path(filename).stem)
+    too_long = filename_length > max_length
+    too_short = filename_length < min_length
     if too_long or too_short:
-        issue_desc = "too_long" if too_long else "too_short"
-        print(f"The length of the name of file '{filename}' is {filename_length}, which is {issue_desc}.")
-        print(f"The length of a filename should be between {LOWER_LIMIT} and {UPPER_LIMIT}.")
-        return 1
-    return 0
+        issue_desc = 'too_long' if too_long else 'too_short'
+        print(
+            f"The length of the name of file '{filename}' is {filename_length}, which is {issue_desc}."
+        )
+        print(
+            f'The length of a filename should be between {min_length} and {max_length}.'
+        )
+        return False
+    return True
